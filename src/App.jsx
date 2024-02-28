a467350d94a60194c19940cada9c169b0af73361
Dario Mercuri <dario.mercuri31@gmail.com>
2024-02-27 20:27:10 -0500

    continued creating sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import {
   motion,
-  useAnimation,
   useInView,
-  useMotionValueEvent,
   useScroll,
   useSpring,
+  useTransform,
 } from "framer-motion";
 import React, { useRef } from "react";
+import { FaEnvelope, FaFileAlt, FaGithub, FaLinkedin } from "react-icons/fa";
 import StickyBox from "react-sticky-box";
 import portrait from "./assets/Portrait.jpg";
 import "./styles/styles.scss";
@@ -20,11 +20,12 @@ function App() {
   ];
 
   const appRef = useRef(null);
-  const { scrollYProgress } = useScroll();
-  const controls = useAnimation();
-
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    console.log("Page scroll: ", latest);
+  const { scrollYProgress } = useScroll({ target: appRef });
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 50,
+    damping: 12,
+    restDelta: 0.001,
+    bounce: 1,
   });
 
   //Refs
@@ -37,12 +38,9 @@ function App() {
   const refProjects = useRef(null);
   const inViewProjects = useInView(refProjects, { margin: "-50%" });
 
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 50,
-    damping: 15,
-    restDelta: 0.001,
-    bounce: 1,
-  });
+  const opacity = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
+  const scale = useTransform(scrollYProgress, [0, 0.5], [0.8, 1]);
+
   return (
     <>
       {appRef && (
@@ -99,13 +97,48 @@ function App() {
                       </motion.a>
                     </motion.li>
                   ))}
+                  <li>
+                    <div className="links">
+                      <motion.a
+                        whileHover={{ scale: 1.5, color: "#9000FF" }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ duration: 0.5 }}
+                        href="https://www.linkedin.com/in/dariomerc/"
+                      >
+                        <FaLinkedin />
+                      </motion.a>
+                      <motion.a
+                        whileHover={{ scale: 1.5, color: "#9000FF" }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ duration: 0.5 }}
+                        href="mailto:dario.mercuri31@gmail.com"
+                      >
+                        <FaEnvelope />
+                      </motion.a>
+                      <motion.a
+                        whileHover={{ scale: 1.5, color: "#9000FF" }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ duration: 0.5 }}
+                        href="https://github.com/DarioMerc"
+                      >
+                        <FaGithub />
+                      </motion.a>
+                      <motion.a
+                        whileHover={{ scale: 1.5, color: "#9000FF" }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ duration: 0.5 }}
+                      >
+                        <FaFileAlt />
+                      </motion.a>
+                    </div>
+                  </li>
                 </ul>
               </nav>
             </div>
           </StickyBox>
           <div className="main-content">
             <motion.section id="About" ref={refAbout}>
-              <div style={{ display: "flex" }}>
+              <div style={{ display: "flex", alignItems: "center" }}>
                 <div>
                   <p>
                     Full-Stack Developer with roughly 2 years of experience. I
@@ -115,47 +148,112 @@ function App() {
                     learning best practices.
                   </p>
                   <p>
-                    Outisde of my career I have strong passions in music and
-                    movies, and also enjoy developing my game.
+                    Outside of my career I enjoy making and listening to music,
+                    watching movies and TV, and developing a video game.
                   </p>
                 </div>
-                <motion.img
-                  src={portrait}
-                  alt=""
-                  className="portrait"
-                  animate={controls}
-                />
+                <motion.img src={portrait} alt="" className="portrait" />
               </div>
             </motion.section>
             <hr />
-            <motion.section id="Experience" ref={refExperience}>
+            <motion.section
+              id="Experience"
+              ref={refExperience}
+              style={{ scale, opacity }}
+            >
               <h2>Experience</h2>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
-              veritatis aliquid iste fuga saepe dolorem, inventore alias!
-              Quibusdam, laborum nam dolorem ea odio sunt deserunt, doloremque
-              aspernatur esse earum praesentium! Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Alias nihil, quam debitis quis ad
-              quisquam. Aut eius voluptates inventore distinctio quasi, totam
-              vitae error sint repudiandae, consectetur, nostrum explicabo
-              perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. In magni perferendis accusantium officiis sed enim ex optio
-              dolore ea nihil illo, odio beatae voluptas et asperiores adipisci.
-              Laboriosam, dolorem recusandae?
+              <div className="job">
+                <div className="header">
+                  <div className="position">Full-Stack Developer</div>
+                  <div className="date">(Nov 2022 - Nov 2023)</div>
+                  <hr />
+                </div>
+                <div className="content">
+                  <ul>
+                    <li>
+                      Independently worked on a variety of projects specializing
+                      in online customer engagement and CMS services for
+                      businesses.
+                    </li>
+                    <li>
+                      Built a prototype SASS application from the ground up in
+                      Angular, .NET and MongoDB.
+                    </li>
+                    <li>
+                      Revamped the look and feel ofmultiple company services in
+                      both Ionic and Material.
+                    </li>
+                    <li>
+                      Planned and executed the integration of a Google Actions
+                      Center API into existing company software.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="job">
+                <div className="header">
+                  <div className="position">Software Developer</div>
+                  <div className="date">(Apr 2022 - Jun 2022)</div>
+                  <hr />
+                </div>
+                <div className="content">
+                  <ul>
+                    <li>
+                      Collaborated with a small agile team to develop, discuss,
+                      and enhance features for multiple backend services using
+                      Laravel PHP.
+                    </li>
+                    <li>
+                      Terminated due to a corporate layoff following the
+                      resignation of the CEO and COO.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="job">
+                <div className="header">
+                  <div className="position">Computer Technician</div>
+                  <div className="date">(May 2020 - Feb 2021)</div>
+                  <hr />
+                </div>
+                <div className="content">
+                  <ul>
+                    <li>
+                      Assisted in building, managing and maintaining hundreds of
+                      computers for cryptocurrency mining.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="job">
+                <div className="header">
+                  <div className="position">Web Developer Intern</div>
+                  <div className="date">(Jan 2020 - Apr 2020)</div>
+                  <hr />
+                </div>
+                <div className="content">
+                  <ul>
+                    <li>
+                      Worked on two client websites on a small agile team in
+                      both front-end and back-end development.
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </motion.section>
             <hr />
             <motion.section id="Education" ref={refEducation}>
               <h2>Education</h2>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
-              veritatis aliquid iste fuga saepe dolorem, inventore alias!
-              Quibusdam, laborum nam dolorem ea odio sunt deserunt, doloremque
-              aspernatur esse earum praesentium! Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Alias nihil, quam debitis quis ad
-              quisquam. Aut eius voluptates inventore distinctio quasi, totam
-              vitae error sint repudiandae, consectetur, nostrum explicabo
-              perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. In magni perferendis accusantium officiis sed enim ex optio
-              dolore ea nihil illo, odio beatae voluptas et asperiores adipisci.
-              Laboriosam, dolorem recusandae?
+              <div className="education">
+                <p>
+                  <strong>Certificate</strong> in{" "}
+                  <strong>Full Stack Development</strong> - Concordia University
+                </p>
+                <p>
+                  <strong>DEC</strong> in <strong>Computer Science</strong> -
+                  John Abbott College
+                </p>
+              </div>
             </motion.section>
             <hr />
             <motion.section id="Projects" ref={refProjects}>
@@ -172,7 +270,38 @@ function App() {
               dolore ea nihil illo, odio beatae voluptas et asperiores adipisci.
               Laboriosam, dolorem recusandae?
             </motion.section>
-            <footer className="footer">Footer</footer>
+            <hr />
+            <section id="Contact">
+              <h2>Contact Me</h2>
+              <form
+                action="https://getform.io/f/YerJRWeO"
+                name="contact"
+                method="post"
+                autoComplete="off"
+              >
+                <input type="hidden" name="form-name" value="contact" />
+                <div>
+                  <label htmlFor="first-name">Name</label> <br />
+                  <input
+                    type="text"
+                    id="first-name"
+                    name="first-name"
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email">Email</label> <br />
+                  <input type="email" id="email" name="email" required />
+                </div>
+                <div>
+                  <label htmlFor="message">Message</label> <br />
+                  <textarea id="message" name="message" required></textarea>
+                </div>
+                <div>
+                  <input type="submit" value="Send" />
+                </div>
+              </form>
+            </section>
           </div>
         </div>
       )}
